d83732cd6642757fc50e416f9cb473e8511c11bd
Ken Holm <kaholm@umc.edu>
2024-12-02 15:19:46 -0600

    Updating the SQL Script

--- files/databaseCreationScript.sql
+++ files/databaseCreationScript.sql
@@ -1,18 +1,20 @@
 --
 -- Use this script to create your database
 --
--- 1. Connect to your local instance of MySQL
---    a. mysql -u <user> -p
--- 2. At the mysql> prompt, use source to read
---    this file and execute the SQL script
---    a. mysql> source databaseCreationScript.sql
+-- 1. Connect to your remote instance of MySQL via MySQLShell
+--    \c cbcradio_bds99@cbcradio.org
+-- 2. At the mysql> prompt, navigate to your database
+--    use cbcradio_bds754_99
+-- 3. Use the source command to execute this SQL script
+--    If your script is saved in the C:\temp folder
+--    > source C:\temp\databaseCreationScript.sql
 --
 
-DROP DATABASE IF EXISTS `housing_project`;
+-- NOTE: Change this to YOUR database
+USE cbcradio_bds754_99;
 
-CREATE DATABASE housing_project;
-
-USE housing_project;
+-- This will delete the table if it exists.
+DROP TABLE IF EXISTS `housing`;
 
 CREATE TABLE housing
                (
@@ -30,3 +32,24 @@ CREATE TABLE housing
                 `median_income`      int not null,
                 `median_house_value` int not null
                );
+
+-- Now, you can use the desc command to see that the table has been added.
+--
+-- > desc housing;
+-- +--------------------+----------+------+-----+---------+----------------+
+-- | Field              | Type     | Null | Key | Default | Extra          |
+-- +--------------------+----------+------+-----+---------+----------------+
+-- | id                 | int(11)  | NO   | PRI | NULL    | auto_increment |
+-- | guid               | char(32) | NO   |     | NULL    |                |
+-- | zip_code           | int(11)  | NO   |     | NULL    |                |
+-- | city               | char(32) | NO   |     | NULL    |                |
+-- | state              | char(2)  | NO   |     | NULL    |                |
+-- | county             | char(32) | NO   |     | NULL    |                |
+-- | median_age         | int(11)  | NO   |     | NULL    |                |
+-- | total_rooms        | int(11)  | NO   |     | NULL    |                |
+-- | total_bedrooms     | int(11)  | NO   |     | NULL    |                |
+-- | population         | int(11)  | NO   |     | NULL    |                |
+-- | households         | int(11)  | NO   |     | NULL    |                |
+-- | median_income      | int(11)  | NO   |     | NULL    |                |
+-- | median_house_value | int(11)  | NO   |     | NULL    |                |
+-- +--------------------+----------+------+-----+---------+----------------+
